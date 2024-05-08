8c33ed641914d84da6009c12535489a085e42b47
Adesh <adeshrai707@gmail.com>
2024-05-08 21:22:47 +0530

    changged

--- profileFinder/src/App.jsx
+++ profileFinder/src/App.jsx
@@ -6,35 +6,37 @@ function App() {
   const [myData, setMyData] = useState([])
   const [profile, setProfile] = useState("")
   const [isError, setIsError] = useState("")
+  const  [display, setDisplay] = useState(false)
   const getUser = () => {
     axios.get(`https://api.github.com/users/${profile}`)
     .then((res) => setMyData(res.data))
     .catch((error) => setIsError(error.message))
+    console.log(isError);
+    setDisplay(true)
   }
   const change = (e)=> 
     setProfile(e.target.value)
   return (
    <>
    {isError != "" && <h2 className='text-slate-200'>{isError}</h2>}
-    <div className='grid  place-content-center mt-[10vh]'>
-      <div className='place-content-evenly'>
+    <div className='  ml-[25%]  w-[50vw]'>
+      <div className=''>
       <input 
       type="text"
       placeholder='Enter UserName'
       value={profile}
       onChange={change}
-      className={`w-1/2 h-10 border-2 rounded-3xl  border-black m-10 mr-0 p-4 bg-slate-600 text-slate-200`}
+      className={`w-[30vw] h-10 border-2 rounded-3xl  border-black m-10 mr-0 p-4 bg-slate-600 text-slate-200`}
       />
       <button 
-      className={`rounded-3xl border-2 h-10 m-3 ml-1 p-1 w-1/5 border-black text-slate-200 text-xl bg-slate-600`}
+      className={`hover:bg-slate-500 hover:border-slate-800 w-[10vw] rounded-3xl border-2 h-10 m-3 ml-1 p-1  border-black text-slate-200 text-xl bg-slate-600`}
       onClick={getUser}>
         Search
       </button>
       </div>
-      <div>
-      <Card name={myData.name} folower={myData.followers} image={myData.avatar_url} />
-      </div>
-    {myData.html_url}
+      { display == true &&  <div>
+      <Card name={myData.name} folower={myData.followers} image={myData.avatar_url} profile={profile} url={myData.html_url}/>
+      </div>}
     </div>
     </>
   )
--- profileFinder/src/components/Card.jsx
+++ profileFinder/src/components/Card.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 
-function Card({name,image,folower}) {
+function Card({name,image,folower,profile,url}) {
   return (
     <>
     <div className='m-10'>
     <div className='grid grid-cols-4 gap-10 w-[50vw] rounded-xl border-2 border-slate-800 bg-slate-700 text-slate-200'>
         <div className='col-span-2'>
-        <img 
+          <a href={url} target='_blank' >
+          <img 
         src={image} 
         width={300}
-        className='rounded-[50%] m-10'
+        className='border-2 border-slate-800 rounded-[50%] m-10'
         />
+          </a>
         </div>
-        <div className='col-span-2'>
+        <div className='col-span-2  grid'>
+          <div className='place-content-center'>
+        <h1 className='text-xl text-left m-4'>Profile: {profile}</h1>
         <h1 className='text-xl text-left m-4'>Name: {name}</h1>
         <h2 className='text-xl text-left m-4'>Followers: {folower}</h2>
+         </div>       
         </div>
     </div>
     </div>
